d2c322abb9edea0e58ac340053399189885f1808
karthika <karthikam@qburst.com>
2015-06-10 16:48:37 +0530

    added page script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 $(function(){
-	var $reset = $('.reset'),
-		$inputField = $('.input-fields'),
-		$input = $inputField.find('input'),
-		$select = $inputField.find('select'),
-		$submit = $('.submit'),
-		$table = $('table');
+	var $reset 			= 		$('.reset'),
+		$inputField 	= 		$('.input-fields'),
+		$input 			= 		$inputField.find('input'),
+		$select 		= 		$inputField.find('select'),
+		$submit 		= 		$('.submit'),
+		$table 			=		$('table'),
+		$searchBtn		=		$('.search'),
+		$refresh 		= 		$('.refresh'),
+		$search 		=		$('.input-search-style');
 
-	$reset.click( function() {
+	function resetting() {
 		$input.val('');
 		$select.find('option[value="male"]').attr("selected",true);
-		$select.find('option[value="programmer"]').attr("selected",true);		
+		$select.find('option[value="programmer"]').attr("selected",true);	
+	}
+
+	$reset.click( function() {
+		resetting();
 	});
 
 	$(".input-fields input[data-input=2], .input-fields input[data-input=5], .input-fields input[data-input=6]").keypress(function (e) {
@@ -45,13 +52,115 @@ $(function(){
   				return;
   			}
   			else{
+
+  				var num = parseInt($table.find('tr:last td:first').text())+1;
+
   				var $values = [];
   				window.alert('ready to submit');
 	  			for( var i=1; i<7; ++i) {
 	  				$values.push($inputField.find('input[data-input='+i+']').val());
 	  			}
+
+	  			if( $( "select[data-input=3] option:selected" ).text().toLowerCase() == 'male' ){
+	  				$values.push('M');
+	  			}
+	  			else{
+	  				$values.push('F');
+	  			}
+	  			$values.push($( "select[data-input=4] option:selected" ).text());
+
+	  			$table.find('tr:last').after('<tr><td>'+num+'</td><td>'+$values[0]+'</td><td>'+$values[1]+'</td><td>'+$values[6]+'</td><td>'+$values[7]+'</td><td>'+$values[4]+'</td><td>'+$values[5]+'K</td></tr>');
+  				resetting();
+
   			} 			
   		}		
 	});
+
+	$searchBtn.click( function() {
+		var searchName = $search.val(),
+			found = false;			
+
+		$table.find('tr').each( function() {
+			
+			var allCells = $(this).find('td:nth-child(2)');
+			if(allCells.length > 0) { 
+
+				var rowIndex = 0;
+
+				allCells.each( function() {
+
+					var regExp = new RegExp(searchName,'i');
+					if(regExp.test(allCells.text())) { 
+						found    = true;
+						rowIndex = allCells.closest('tr').index();
+						return false;
+					}
+
+				} );
+
+				if(found == true) {  console.log(rowIndex);
+					$table.find('tr:eq('+rowIndex+')').show(); 					
+				}
+				else {
+					$table.find('tr:not(:first-child)').hide();
+				}
+
+			}
+
+		} );
+	});
+
+	$refresh.click( function() {
+		$search.val('');
+		$table.find('tr').show();
+	});
+
+	$('table tr').on( 'click', function(event) {
+		var target = $( event.target );
+
+		if ( target.is( "th:nth-child(2)" ) ) {
+			sort(2);
+		}
+		else if ( target.is( "th:nth-child(3)" ) ) {
+			sort(3);
+		}
+		else if ( target.is( "th:nth-child(4)" ) ) {
+			sort(4);
+		}
+		else if ( target.is( "th:nth-child(5)" ) ) {
+			sort(5);
+		}
+		else if ( target.is( "th:nth-child(6)" ) ) {
+			sort(6);
+		}
+		else if ( target.is( "th:nth-child(7)" ) ) {
+			sort(7);
+		}
+	} );
+
+
+	var sort = function( index ) {
+		var i=0,
+			$colValues = [];
+
+		$table.find('tr').each( function() {
+			var	allCells = $(this).find('td:nth-child('+index+')');
+
+			allCells.each( function() {
+				$colValues.push(allCells.text());
+			});
+		});	
+
+		// $colValues.sort();
+
+		$colValues.sort(function (a, b) {
+		    return a.toLowerCase().localeCompare(b.toLowerCase());
+		});
+	
+		$table.find('tr td:nth-child('+index+')').each( function(){
+			$(this).html($colValues[i]);
+			i++;
+		} );
+	}
 
 });
